0b211c8a4effecffe1220ce46f44500938324dae
fefe2200 <dlcjf2200@naver.com>
2022-11-20 18:02:42 +0900

    feat, test : requestClassTest파일 생성 및 testCode 파일 위치 이동

---
+++ testCode/requestClassTest.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include "../Request.hpp"
+
+    //map에서의 iterator를 사용한 요소 접근의 경우 iter->first, iter->second와 같은 형식으로
+    //사용할 수 있다.
+
+void	testFunc(std::string & data, std::string &status, std::string resultStatus)
+{
+	Request request;
+
+	request.parse(data, status);
+	if (status == resultStatus)
+	{
+		std::cout << "test OK!! " << std::endl;
+		return ;
+	}
+	else
+		std::cout << "errer!!  status : resultStatus = " << status << " : " << resultStatus << std::endl << std::endl;
+
+	std::cout << "Method : " << request.getMethod() << std::endl;
+	std::cout << "Target : " << request.getTarget() << std::endl;
+	std::cout << "Version : " << request.getVersion() << std::endl;
+	std::cout << "Headers: " << std::endl;
+	std::map<std::string, std::string> temp = request.getHeaders();
+	std::map<std::string,std::string>::iterator iter;
+	for(iter = temp.begin() ; iter != temp.end(); iter++){
+		std::cout << iter->first << " : "<< iter->second << std::endl;
+	}
+	std::cout << "Body : " << request.getBody() << std::endl;
+
+}
+
+int main(void)
+{
+	std::string data;
+	std::string status;
+
+	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "200"); //기본 GET 성공
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "DELETE / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "200"); //기본 DELETE 성공
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "PUT / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "405");// 지원하지 않는 메소드 PUT
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "200");//마지막 헤더 문자열에 \r\n추가
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "200");//마지막에 \r\n 만 있는 문자열 추가
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "GET / HTTP/1.0\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "505");//지원하지 않는 HTTP 버전
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n123";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "400");//get 메소드에 바디 추가
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n123";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "400");//POST 메소드에 헤더 바디사이에 빈 문자열 없이 바디 추가
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n123\r\n456";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "200");//POST 메소드에 바디 개행이 \r\n 이 경우 설정
+	std::cout << "---------------------------------------------------" << std::endl;
+
+	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n123\n456";
+	std::cout << "---------------------------------------------------" << std::endl;
+	testFunc(data, status, "200");//POST 메소드에 바디 개행이 \n 이 경우 설정
+	std::cout << "---------------------------------------------------" << std::endl;
+	return (0);
+}
