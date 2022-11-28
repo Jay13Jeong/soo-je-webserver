888734cdd838c5e10cabc9d5ae66a7c3df88d8f5
fefe2200 <dlcjf2200@naver.com>
2022-11-29 03:49:59 +0900

    chore: request 시간복잡도 개선

--- Request.hpp
+++ Request.hpp
@@ -35,21 +35,40 @@ private:
         return (status_code = "800", false);
     }
 private:
-    bool ft_chunk_push_body(std::vector<std::string> temp_data, int i, std::string &status_code)
+    bool ft_chunk_push_body(std::string temp_data, std::string &status_code)
     {
         std::string temp = "";
-        for (int j = i + 1; j < temp_data.size(); j+=2)//바디부터 시작
+        size_t count;
+        for (int i = 0; i < temp_data.size();)//바디부터 시작
         {
-            if (j + 1 >= temp_data.size())
+            count = strtol(temp_data.substr(i).c_str(), NULL, 16);//16진수로 읽을 수 있는 곳까지 숫자를 읽음
+
+            i = i + temp_data.substr(i).find("\r\n");//청크 길이 뒤에 처음오는 \r\n 시작위치
+            if (i == std::string::npos)
                 return (status_code = "400", false);
-            if (strtol(temp_data[j].c_str(), NULL, 16) != temp_data[j + 1].size())
+            i += 2;//\r\n건너띄기
+
+            temp = temp + temp_data.substr(i, count);//바디 추가
+
+            if (i + count != i + temp_data.substr(i).find("\r\n"))//청크 바디 뒤에 \r\n 확인
                 return (status_code = "400", false);
-            temp = temp + temp_data[j + 1];
+            i = i + count + 2;//다음
         }
         setBody(temp);
         return (status_code = "200", true);
     }
+private:
+    size_t ft_find_header_end(std::string data)
+    {
+        size_t num = data.find("\r\n\r\n");//헤더와 바디 사이의 공백 찾기, 헤더의 마지막 부분.
 
+        if (num != std::string::npos)
+            return num;
+        num = data.rfind("\r\n");//\r\n이 중간에 없으면 끝에서 부터 찾기 시작, 바디가 없다고 추측, 헤더가 \r\n으로 끝날때
+        if (num != data.size() - 2)//끝에 \r\n 이 없는 경우
+            return data.size();
+        return num;
+    }
 public:
     std::string getMethod()
     {
@@ -116,14 +135,18 @@ public:
         // // std::cerr << "request.parse() 함수에 들어온 데이터"<< std::endl;
         // // std::cerr << data << std::endl;
         // // std::cerr << "여기까지"<< std::endl;
-        std::vector<std::string> temp_data = util::ft_split_s(data, "\r\n");
+        size_t data_header_end_point = ft_find_header_end(data);//헤더 마지막 부분 찾기
+        if (data_header_end_point == 0)
+            return (status_code = "400", false);
+
+        std::vector<std::string> temp_data = util::ft_split_s(data.substr(0, data_header_end_point), "\r\n");
         std::vector<std::string> temp_str;
         std::string temp = "";
         status_code = "";//초기화
 
         //스타트라인
         temp_str = util::ft_split_s(temp_data[0], " ");
-        if (temp_str.size() != 3)
+        if (temp_str.size() != 3)//스타트라인 규격 체크
             return (status_code = "400", false);
         else if (temp_str[0] == "GET" || temp_str[0] == "DELETE" || temp_str[0] == "POST" || temp_str[0] == "PUT" || temp_str[0] == "HEAD")
             setMethod(temp_str[0]);
@@ -132,7 +155,7 @@ public:
 
         if (temp_str[2] == "HTTP/1.1")
             setVersion(temp_str[2]);
-        else if (!(temp_str[2] == "HTTP/1.1"))
+        else if (!(temp_str[2] == "HTTP/1.1"))//HTTP/1.1만 지원
             return (status_code = "505", false);
         else
             return (status_code = "400", false);
@@ -162,29 +185,33 @@ public:
             setHeaders(temp_str[0], temp);
             i++;
         }
-        if (i == 1)
+        if (i == 1)//헤더가 없는 경우,temp_data.size()==i아니면 헤더 파싱 다 된 것 아님.
             return (status_code = "400", false);
-        else if ((getMethod() == "GET" || getMethod() == "DELETE" || temp_str[0] == "HEAD") && !(i == temp_data.size() || i == temp_data.size() - 1))
+        else if ((getMethod() == "GET" || getMethod() == "DELETE" || getMethod() == "HEAD") && !(data_header_end_point >= (data.size() - 4)))//-4는 올수 있는CRLF의 최대값
             return (status_code = "400", false);
         // else if ((getMethod() == "POST") && (i == temp_data.size() || i == temp_data.size() - 1))// Post에서 바디가 없을 수도 있다
         //     return (status_code = "411", false);
 
         //바디부분
         if (status_code == "800")
-            return (ft_chunk_push_body(temp_data, i, status_code));
-        temp = "";
-        while (i < temp_data.size())
-        {
-            temp = temp + temp_data[i];
-            //temp = temp + "\r\n";
-            i++;
-        }
+            return (ft_chunk_push_body(data.substr(data_header_end_point + 4), status_code));//바디 처음부터 시작
 
-        if (this->getHeaders().find("Content-Length") == this->getHeaders().end())
+        temp = "";
+        //while (i < temp_data.size())
+        //{
+        //    temp = temp + temp_data[i];
+        //    //temp = temp + "\r\n";
+        //    i++;
+        //}
+
+        if (data.size() > data_header_end_point + 4)//바디 시작부분이 요청값 전체 사이즈 보다 작아야 한다.
+            temp = temp + data.substr(data_header_end_point + 4);//바디 부분 시작지점.+4 "\r\n\r\n"이후 바디 첫번쨰 값.
+
+        if (this->getHeaders().find("Content-Length") == this->getHeaders().end())//"Content-Length"없음
             setBody(temp);
-        else if (strtol(this->getHeaders()["Content-Length"].c_str(), NULL, 10) >= temp.size())
+        else if (strtol(this->getHeaders()["Content-Length"].c_str(), NULL, 10) >= temp.size())//"Content-Length"길이가 더 큼
             setBody(temp);
-        else if (strtol(this->getHeaders()["Content-Length"].c_str(), NULL, 10) < temp.size())
+        else if (strtol(this->getHeaders()["Content-Length"].c_str(), NULL, 10) < temp.size())//바디 크기가 더크면 잘라서 넣기
             setBody(temp.substr(0, strtol(this->getHeaders()["Content-Length"].c_str(), NULL, 10)));
 
         return (status_code = "200", true); //문제없으면 true반환;
--- testCode/requestClassTest.cpp
+++ testCode/requestClassTest.cpp
@@ -11,22 +11,22 @@ void	testFunc(std::string & data, std::string &status, std::string resultStatus)
 	request.parse(data, status);
 	if (status == resultStatus)
 	{
-		// std::cerr << "test OK!! " << std::endl;
+		std::cerr << "test OK!! " << std::endl;
 		return ;
 	}
 	else
-		// std::cerr << "errer!!  status : resultStatus = " << status << " : " << resultStatus << std::endl << std::endl;
+		std::cerr << "errer!!  status : resultStatus = " << status << " : " << resultStatus << std::endl << std::endl;
 
-	// std::cerr << "Method : " << request.getMethod() << std::endl;
-	// std::cerr << "Target : " << request.getTarget() << std::endl;
-	// std::cerr << "Version : " << request.getVersion() << std::endl;
-	// std::cerr << "Headers: " << std::endl;
+	std::cerr << "Method : " << request.getMethod() << std::endl;
+	std::cerr << "Target : " << request.getTarget() << std::endl;
+	std::cerr << "Version : " << request.getVersion() << std::endl;
+	std::cerr << "Headers: " << std::endl;
 	std::map<std::string, std::string> temp = request.getHeaders();
 	std::map<std::string,std::string>::iterator iter;
 	for(iter = temp.begin() ; iter != temp.end(); iter++){
-		// std::cerr << iter->first << " : "<< iter->second << std::endl;
+		 std::cerr << iter->first << ":"<< iter->second << std::endl;
 	}
-	// std::cerr << "Body : " << request.getBody() << std::endl;
+	std::cerr << "Body : " << request.getBody() << std::endl;
 
 }
 
@@ -36,83 +36,83 @@ int main(void)
 	std::string status;
 
 	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
-	// std::cerr << "1---------------------------------------------------" << std::endl;
+	std::cerr << "1---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200"); //기본 GET 성공
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "DELETE / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
-	// std::cerr << "2---------------------------------------------------" << std::endl;
+	std::cerr << "2---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200"); //기본 DELETE 성공
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "PUT / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
-	// std::cerr << "3---------------------------------------------------" << std::endl;
+	std::cerr << "3---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");// 지원하지 않는 메소드 PUT// 지금은 지원함
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n";
-	// std::cerr << "4---------------------------------------------------" << std::endl;
+	std::cerr << "4---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//마지막 헤더 문자열에 \r\n추가
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n";
-	// std::cerr << "5---------------------------------------------------" << std::endl;
+	std::cerr << "5---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//마지막에 \r\n 만 있는 문자열 추가
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "GET / HTTP/1.0\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9";
-	// std::cerr << "6---------------------------------------------------" << std::endl;
+	std::cerr << "6---------------------------------------------------" << std::endl;
 	testFunc(data, status, "505");//지원하지 않는 HTTP 버전
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n123";
-	// std::cerr << "7---------------------------------------------------" << std::endl;
+	std::cerr << "7---------------------------------------------------" << std::endl;
 	testFunc(data, status, "400");//get 메소드에 바디 추가
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n123";
-	// std::cerr << "8---------------------------------------------------" << std::endl;
+	std::cerr << "8---------------------------------------------------" << std::endl;
 	testFunc(data, status, "400");//POST 메소드에 헤더 바디사이에 빈 문자열 없이 바디 추가
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n123\r\n456";
-	// std::cerr << "9---------------------------------------------------" << std::endl;
+	std::cerr << "9---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//POST 메소드에 바디 개행이 \r\n 이 경우 설정
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9\r\n\r\n123\n456";
-	// std::cerr << "10---------------------------------------------------" << std::endl;
+	std::cerr << "10---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//POST 메소드에 바디 개행이 \n 이 경우 설정
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate, br\r\nTransfer-Encoding: chunked, gzip\r\n\r\n";
-	// std::cerr << "11---------------------------------------------------" << std::endl;
+	std::cerr << "11---------------------------------------------------" << std::endl;
 	testFunc(data, status, "800");//POST 청크 확인. 바디 없음
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate, br\r\nTransfer-Encoding: gzip\r\n\r\n";
-	// std::cerr << "12---------------------------------------------------" << std::endl;
+	std::cerr << "12---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//POST 청크 확인. 바디 없음, 헤더에 청크값 없음
 	// std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate, br\r\nTransfer-Encoding: chunked, gzip\r\n\r\n1\r\n1\r\n0\r\n\r\n";
-	// std::cerr << "13---------------------------------------------------" << std::endl;
+	std::cerr << "13---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//POST 청크 확인. 바디 1줄 있음
-	// std::cerr << "---------------------------------------------------" << std::endl;
+	 std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate, br\r\nTransfer-Encoding: chunked\r\n\r\n0\r\n\r\n";
-	// std::cerr << "14---------------------------------------------------" << std::endl;
+	std::cerr << "14---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//POST 청크 확인. 바디 없이 바로 종료청크 올때
-	// std::cerr << "---------------------------------------------------" << std::endl;
+	 std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate, br\r\nTransfer-Encoding: chunked, gzip\r\n\r\n1\r\n1\r\n9\r\n1q2q3q\n4q\r\n0\r\n\r\n";
-	// std::cerr << "15---------------------------------------------------" << std::endl;
+	std::cerr << "15---------------------------------------------------" << std::endl;
 	testFunc(data, status, "200");//POST 청크 확인. 바디 있음 여러줄,, 자음이 오면 하나당 길이 2
-	// std::cerr << "---------------------------------------------------" << std::endl;
+	 std::cerr << "---------------------------------------------------" << std::endl;
 
 	data = "POST / HTTP/1.1\r\nHost: localhost:8080\r\nConnection: keep-alive\r\nAccept-Encoding: gzip, deflate, br\r\nTransfer-Encoding: chunked\r\n\r\n1\r\n1\r\n8\r\n1q2w3e4\r\n0\r\n\r\n";
-	// std::cerr << "16---------------------------------------------------" << std::endl;
+	std::cerr << "16---------------------------------------------------" << std::endl;
 	testFunc(data, status, "400");//POST 청크 확인.청크길이와 문자열 길이가 다른 경우
-	// std::cerr << "---------------------------------------------------" << std::endl;
+	 std::cerr << "---------------------------------------------------" << std::endl;
 	return (0);
 }
